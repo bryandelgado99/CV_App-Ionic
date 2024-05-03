4d53aed22bbbebb783090e7e4e939045e96d5497
Bryan Paul Delgado <bryand9970@gmail.com>
2024-05-03 16:44:11 -0500

    Android Assets, Capactior and Ionic App created

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ionic.cvapp',
+  appId: 'com.cv.app',
   appName: 'cvapp',
   webDir: 'www'
 };
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItemDivider, IonText, IonCard, IonGrid, IonCol, IonRow, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonIcon, IonButton, IonCardContent, IonCardTitle, IonCardHeader, IonRow, IonCol, IonGrid, IonCard, IonText, IonItemDivider, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Tab2Page {
 
